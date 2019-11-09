e10cd0818c3240b9e8abd20e19cb1032b89d865f
reagustin <re_agustin@hotmail.com>
2019-11-09 19:49:17 -0300

    Agregando todo

---
+++ WindowsFormsApplication2/Lote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Lote
+    {
+        public Empresa empresa { get; set; }
+        public List<DetalleLote> detallelote { get; set; }
+
+        public Lote()
+        {
+            empresa = new Empresa();
+            detallelote = new List<DetalleLote>();
+        }
+    }
+}
